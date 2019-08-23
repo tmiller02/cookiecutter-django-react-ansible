df80efc9e640d49a9bf68476bd1e465d3d3a1891
Tom <02millert@gmail.com>
2019-08-23 20:54:47 +0100

    Set allowed hosts in the backend_app role.

--- {{ cookiecutter.project_slug }}/backend_app/{{ cookiecutter.project_slug }}/settings.py
+++ {{ cookiecutter.project_slug }}/backend_app/{{ cookiecutter.project_slug }}/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition

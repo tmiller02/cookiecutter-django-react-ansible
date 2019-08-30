a5dce3944ac122e335826c515d4920116cfc8901
Tom <02millert@gmail.com>
2019-08-30 23:19:09 +0100

    Add a separate rc file. Update the vagrant_ssh.sh script to accept additional arguments.

--- {{ cookiecutter.project_slug }}/vagrant_ssh.sh
+++ {{ cookiecutter.project_slug }}/vagrant_ssh.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
 
-activateCommand='/bin/bash --init-file <(echo ". \"$HOME/.bashrc\"; cd /opt/{{ cookiecutter.project_slug }}_backend/current; source venv/bin/activate")'
-vagrant ssh -c "sudo -iu {{ cookiecutter.project_slug }} /bin/bash -c '$activateCommand'"
+# A helper script to quickly ssh onto the dev VM as the correct user, in the
+# backend app directory, with the virtualenv activated.
+#
+# Example usage:
+#
+#  To just ssh onto the VM from your host machine into an interactive shell:
+#
+#    $ ./vagrant_ssh.sh
+#
+#  To run the backend unit tests:
+#
+#    $ ./vagrant_ssh.sh make test
+#
+#  To enter the django shell:
+#
+#    $ ./vagrant_ssh.sh python manage.py shell
+
+if [ $# -eq 0 ]
+  then
+    vagrant ssh -c "sudo -iu {{ cookiecutter.project_slug }} bash --rcfile .venvrc -i";
+  else
+    vagrant ssh -c "sudo -iu {{ cookiecutter.project_slug }} bash -c '. ~/.venvrc && $*'";
+fi

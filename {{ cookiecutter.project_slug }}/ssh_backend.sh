70961dbf25552f11757656c05f7d6cdce146aee5
Tom <02millert@gmail.com>
2019-09-13 22:57:20 +0100

    Update Jenkinsfile with stages to run frontend checks and tests.

--- {{ cookiecutter.project_slug }}/ssh_backend.sh
+++ {{ cookiecutter.project_slug }}/ssh_backend.sh
@@ -7,19 +7,19 @@
 #
 #  To just ssh onto the VM from your host machine into an interactive shell:
 #
-#    $ ./vagrant_ssh.sh
+#    $ ./ssh_backend.sh
 #
 #  To run the backend unit tests:
 #
-#    $ ./vagrant_ssh.sh make test
+#    $ ./ssh_backend.sh make test
 #
 #  To enter the django shell:
 #
-#    $ ./vagrant_ssh.sh python manage.py shell
+#    $ ./ssh_backend.sh python manage.py shell
 #
 #  To start firefox via X11 forwarding (assuming you have a running X11 server):
 #
-#    $ ./vagrant_ssh.sh firefox
+#    $ ./ssh_backend.sh firefox
 #
 
 if [ $# -eq 0 ]
@@ -32,7 +32,7 @@ if [ $# -eq 0 ]
       sudo -i -u {{ cookiecutter.project_slug }} bash --rcfile .venvrc";
   else
     vagrant ssh -c "
-      if test -f '/home/vagrant/.Xauthority; then
+      if test -f '/home/vagrant/.Xauthority'; then
         XAUTH_COOKIE=\$(xauth list | tail -n 1) &&
         sudo -u {{ cookiecutter.project_slug }} xauth add \$XAUTH_COOKIE
       fi

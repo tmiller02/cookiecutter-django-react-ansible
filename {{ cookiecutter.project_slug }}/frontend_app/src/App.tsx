d19840b6acfef5b6a46cc8c338b1390959a09daf
Tom <02millert@gmail.com>
2019-09-16 22:04:18 +0100

    Add react-router and prettier to the frontend app.
    Update nginx configuration for react-router.

--- {{ cookiecutter.project_slug }}/frontend_app/src/App.tsx
+++ {{ cookiecutter.project_slug }}/frontend_app/src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  RouteComponentProps,
+  Switch
+} from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +27,27 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
+
+const NoMatch: React.FC<RouteComponentProps> = ({ location }) => {
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;

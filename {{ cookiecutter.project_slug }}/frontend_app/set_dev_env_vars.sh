e0117cf90bac34ffbf5f3834b1c20f6ac30d5b71
Tom <02millert@gmail.com>
2019-09-22 10:58:37 +0100

    Update frontend app documentation.

--- {{ cookiecutter.project_slug }}/frontend_app/set_dev_env_vars.sh
+++ {{ cookiecutter.project_slug }}/frontend_app/set_dev_env_vars.sh
@@ -3,13 +3,10 @@
 export REACT_APP_BACKEND_URL="https://{{ cookiecutter.backend_app_dev_hostname }}"
 
 # See https://create-react-app.dev/docs/advanced-configuration
-# Adjust configuration as required when running the frontend app dev server from
-# the controller VM.
+# Adjust configuration as required when running on the controller VM.
 
 CONTROLLER_HOSTNAME="{{ cookiecutter.controller_hostname }}"
 if [ "$(uname -n)" = $CONTROLLER_HOSTNAME ]; then
   export HOST=$CONTROLLER_HOSTNAME
   export CHOKIDAR_USEPOLLING=true
 fi
-
-yarn run start

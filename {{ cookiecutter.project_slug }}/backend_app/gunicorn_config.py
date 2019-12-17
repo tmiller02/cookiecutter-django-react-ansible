5c86d5f21f130257f3ce997ad2fc4ea8586ce06b
Tom Miller <tom.miller@bbc.co.uk>
2019-12-17 10:38:44 +0000

    Load gunicorn config from the .env file. Move the gunicorn_config.py file from an ansible template to a regular python file in the backend app.

---
+++ {{ cookiecutter.project_slug }}/backend_app/gunicorn_config.py
@@ -0,0 +1,11 @@
+import os
+import environ
+from multiprocessing import cpu_count
+
+BASE_DIR = os.path.dirname((os.path.abspath(__file__)))
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, "settings.env"))
+
+bind = env.str("GUNICORN_BIND")
+pidfile = env.str("GUNICORN_PID_FILE")
+workers = cpu_count() * 2 + 1

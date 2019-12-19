587674a614195072645896fe6421ea4c14ef44f1
Tom <02millert@gmail.com>
2019-12-19 21:37:21 +0000

    Set 'vagrant' as the backend app user in the dev environment.

--- {{ cookiecutter.project_slug }}/ssh_backend.sh
+++ {{ cookiecutter.project_slug }}/ssh_backend.sh
@@ -31,18 +31,8 @@ fi
 
 if [ $# -eq 0 ]
   then
-    vagrant ssh -c "
-      if test -f '/home/vagrant/.Xauthority'; then
-        XAUTH_COOKIE=\$(xauth list | tail -n 1) &&
-        sudo -u {{ cookiecutter.project_slug }} xauth add \$XAUTH_COOKIE
-      fi
-      sudo -i -u {{ cookiecutter.project_slug }} bash --rcfile .venvrc";
+    vagrant ssh -c "bash --rcfile .venvrc";
   else
-    vagrant ssh -c "
-      if test -f '/home/vagrant/.Xauthority'; then
-        XAUTH_COOKIE=\$(xauth list | tail -n 1) &&
-        sudo -u {{ cookiecutter.project_slug }} xauth add \$XAUTH_COOKIE
-      fi
-      sudo -i -u {{ cookiecutter.project_slug }} bash -c '. ~/.venvrc && $*'
-    ";
+    vagrant ssh -c "bash -c '. ~/.venvrc && $*'";
 fi
+

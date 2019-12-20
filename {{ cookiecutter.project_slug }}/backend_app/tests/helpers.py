b0b5cff280e3d373142dea53fe1e5932e0c50742
Tom Miller <tom.miller@bbc.co.uk>
2019-12-20 10:37:06 +0000

    Explicitly set the path for the firefox binary used in selenium tests.

--- {{ cookiecutter.project_slug }}/backend_app/tests/helpers.py
+++ {{ cookiecutter.project_slug }}/backend_app/tests/helpers.py
@@ -1,5 +1,6 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.conf import settings
@@ -16,6 +17,7 @@ class SeleniumLiveServerTestCase(StaticLiveServerTestCase):
             options.headless = True
 
         cls.selenium = Firefox(
+            firefox_binary=FirefoxBinary(firefox_path='/usr/bin/firefox'),
             executable_path="/usr/local/bin/geckodriver", options=options
         )
         cls.selenium.implicitly_wait(10)

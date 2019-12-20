116bdf2a80de08c5c9c6cab59a8ace9b37c3e6f7
Tom Miller <tom.miller@bbc.co.uk>
2019-12-20 14:06:03 +0000

    Add composite run config for frontend and backend app.

--- {{ cookiecutter.project_slug }}/frontend_app/set_dev_env_vars.sh
+++ {{ cookiecutter.project_slug }}/frontend_app/set_dev_env_vars.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-export REACT_APP_BACKEND_URL="https://{{ cookiecutter.backend_app_dev_hostname }}"
+# Set the default value for the backend url environment variable to point to
+# the backend app in the 'dev' environment being served by Nginx. You can
+# override this by setting the REACT_APP_BACKEND_URL environment variable.
+REACT_APP_BACKEND_URL=${REACT_APP_BACKEND_URL:-"https://{{ cookiecutter.backend_app_dev_hostname }}"}
+export REACT_APP_BACKEND_URL
 
 # See https://create-react-app.dev/docs/advanced-configuration
 # Adjust configuration as required when running on the controller VM.
-
 CONTROLLER_HOSTNAME="{{ cookiecutter.controller_hostname }}"
 if [ "$(uname -n)" = $CONTROLLER_HOSTNAME ]; then
   export HOST=$CONTROLLER_HOSTNAME

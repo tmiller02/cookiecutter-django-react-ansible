be535a21ad217b26971fb7c4594259a2c087dfbd
Tom <02millert@gmail.com>
2020-01-06 21:01:39 +0000

    Define default REACT_APP_BACKEND_URL using a .env file. Move environment variable sanity check into index.tsx.

--- {{ cookiecutter.project_slug }}/frontend_app/src/index.tsx
+++ {{ cookiecutter.project_slug }}/frontend_app/src/index.tsx
@@ -3,4 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+const requiredEnvironmentVariables = [
+  "REACT_APP_BACKEND_URL",
+];
+
+const unsetEnvironmentVariables = requiredEnvironmentVariables.filter(
+  (environmentVariable) => { return !(environmentVariable in process.env) }
+);
+
+if (unsetEnvironmentVariables.length > 0) {
+  throw new Error(
+    `The following required environment variables are not set: ${unsetEnvironmentVariables.join(
+      ", "
+    )}`
+  );
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));

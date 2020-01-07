0e3701f5be74d478da38b43481033c748a271694
Tom <02millert@gmail.com>
2020-01-07 22:40:14 +0000

    Add comment about environment variables in the set_dev_env_vars.sh script.

--- {{ cookiecutter.project_slug }}/frontend_app/set_dev_env_vars.sh
+++ {{ cookiecutter.project_slug }}/frontend_app/set_dev_env_vars.sh
@@ -2,6 +2,11 @@
 
 # See https://create-react-app.dev/docs/advanced-configuration
 # Adjust configuration as required when running on the controller VM.
+#
+# Note that other variables are set using .env files, see
+# https://create-react-app.dev/docs/adding-custom-environment-variables/#what-other-env-files-can-be-used
+# as well as via regular environment variables in some PyCharm run configurations.
+
 CONTROLLER_HOSTNAME="{{ cookiecutter.controller_hostname }}"
 if [ "$(uname -n)" = $CONTROLLER_HOSTNAME ]; then
   export HOST=$CONTROLLER_HOSTNAME

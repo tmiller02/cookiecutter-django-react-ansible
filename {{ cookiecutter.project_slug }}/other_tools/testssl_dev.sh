ef0f7b2105374498f90ea0ee97236da4ff415e84
Tom <02millert@gmail.com>
2021-01-09 15:20:32 +0000

    Use set -e in testssh.sh helper script.

--- {{ cookiecutter.project_slug }}/other_tools/testssl_dev.sh
+++ {{ cookiecutter.project_slug }}/other_tools/testssl_dev.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
+set -e
+
 TESTSSL_REPO=https://github.com/drwetter/testssl.sh.git
-SCRIPT_DIR="$( cd "$(dirname "$0")" || exit ; pwd -P )"
+SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 REPO_DIR="${SCRIPT_DIR}/testssl.sh"
 
 if [ ! -d "$REPO_DIR/.git" ]
   then
     git clone "$TESTSSL_REPO" "$REPO_DIR"
   else
-    cd "$REPO_DIR" || exit
+    cd "$REPO_DIR"
     git pull "$TESTSSL_REPO"
 fi
 
-cd "$REPO_DIR" || exit
+cd "$REPO_DIR"
+
+set +e
 
 ./testssl.sh --full https://localhost:4000
 ./testssl.sh --full https://localhost:5000

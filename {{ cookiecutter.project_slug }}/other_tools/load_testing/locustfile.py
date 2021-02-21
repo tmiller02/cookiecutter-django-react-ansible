ae63da42554abc99ef9914bfff4e2cd01503b26f
Tom <02millert@gmail.com>
2021-02-21 10:36:16 +0000

    Update locust requirements.

--- {{ cookiecutter.project_slug }}/other_tools/load_testing/locustfile.py
+++ {{ cookiecutter.project_slug }}/other_tools/load_testing/locustfile.py
@@ -1,14 +1,11 @@
-from locust import HttpLocust, TaskSet, between, task
-from locust.contrib.fasthttp import FastHttpLocust
+from locust import between, task
+from locust.contrib.fasthttp import FastHttpUser
 
 
-class UserBehavior(TaskSet):
+class WebsiteUser(FastHttpUser):
+
+    wait_time = between(5.0, 9.0)
 
     @task
     def get_index(self):
         self.client.get("/admin/")
-
-
-class WebsiteUser(FastHttpLocust):
-    task_set = UserBehavior
-    wait_time = between(5.0, 9.0)

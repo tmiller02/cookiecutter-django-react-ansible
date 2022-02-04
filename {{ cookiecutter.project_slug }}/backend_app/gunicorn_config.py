5a532d4108d82035aeaa875be2c337c97497ef5b
Tom Miller <tom.miller@bbc.co.uk>
2022-02-04 13:55:12 +0000

    Use uvicorn worker with gunicorn.

--- {{ cookiecutter.project_slug }}/backend_app/gunicorn_config.py
+++ {{ cookiecutter.project_slug }}/backend_app/gunicorn_config.py
@@ -8,4 +8,5 @@ env.read_env(os.path.join(BASE_DIR, "settings.env"))
 
 bind = env.str("GUNICORN_BIND")
 pidfile = env.str("GUNICORN_PID_FILE")
+worker_class = "uvicorn.workers.UvicornWorker"
 workers = cpu_count() * 2 + 1

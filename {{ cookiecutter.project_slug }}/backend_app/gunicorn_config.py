852922bb9da8fccd0b58a8907df5b67de8ceb8dd
Tom Miller <tom.miller@bbc.co.uk>
2022-02-04 14:21:50 +0000

    Update gunicorn config to use a uvicorn worker. Update gunicorn systemd unit file from updated example in gunicorn documentation.

--- {{ cookiecutter.project_slug }}/backend_app/gunicorn_config.py
+++ {{ cookiecutter.project_slug }}/backend_app/gunicorn_config.py
@@ -7,6 +7,5 @@ env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, "settings.env"))
 
 bind = env.str("GUNICORN_BIND")
-pidfile = env.str("GUNICORN_PID_FILE")
 worker_class = "uvicorn.workers.UvicornWorker"
 workers = cpu_count() * 2 + 1

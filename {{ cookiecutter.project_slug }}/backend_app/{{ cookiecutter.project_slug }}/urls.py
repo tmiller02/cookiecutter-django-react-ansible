deb0d5a42a02dbdd366e802f84713ab3e6b1c01a
Tom <02millert@gmail.com>
2022-09-07 23:00:57 +0100

    Add default urlconf.

--- {{ cookiecutter.project_slug }}/backend_app/{{ cookiecutter.project_slug }}/urls.py
+++ {{ cookiecutter.project_slug }}/backend_app/{{ cookiecutter.project_slug }}/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
+from django.views import debug
 
 urlpatterns = [
+    path("", debug.default_urlconf),
     path("admin/", admin.site.urls),
 ]
 

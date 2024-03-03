bbb26c413d0e470c962fa1eb2a960254fff9a934
Tom <02millert@gmail.com>
2024-03-03 09:58:03 +0000

    Add separate 'developer' user in Dockerfile, use password based ssh for podman dev environment.

--- {{ cookiecutter.project_slug }}/provision_podman.sh
+++ {{ cookiecutter.project_slug }}/provision_podman.sh
@@ -1,4 +1,4 @@
-podman compose --file compose.yaml exec provisioner bash -c '
+podman compose --file compose.yaml exec --user developer provisioner bash -c '
   sudo apt-get update
   sudo apt-get install -y curl rsync python3 python3-venv python3-pip python3-apt libssl-dev
   python3.10 -m venv ~/provisioning_venv --system-site-packages
@@ -6,6 +6,8 @@ podman compose --file compose.yaml exec provisioner bash -c '
   pip install pip==23.3.1 wheel==0.41.3
   pip install -r /workspace/provisioning/requirements.txt --no-deps
   cd /workspace/provisioning/
-  ~/provisioning_venv/bin/ansible-galaxy install --role-file=requirements.yml
-  ANSIBLE_CONFIG=ansible_vagrant.cfg ~/provisioning_venv/bin/ansible-playbook playbook.yml -i environments/dev/inventory
+  sudo ~/provisioning_venv/bin/ansible-galaxy install --role-file=requirements.yml
+  ANSIBLE_CONFIG=ansible_vagrant.cfg ~/provisioning_venv/bin/ansible-playbook playbook.yml \
+   --inventory environments/dev/inventory \
+   --extra-vars "ansible_user=developer ansible_password=developer"
 '
